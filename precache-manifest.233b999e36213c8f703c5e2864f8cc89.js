29390c5419e5c7f42027fe39d6d333b6d4c6e862
Tureckiy <ev.zhdanov@gmail.com>
2020-06-28 22:39:02 +0300

    Updates

---
+++ precache-manifest.233b999e36213c8f703c5e2864f8cc89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b5266c30f90f6e164d20dca696f705",
+    "url": "/imageFinder/index.html"
+  },
+  {
+    "revision": "623dacd6f9c5e7e5daa5",
+    "url": "/imageFinder/static/css/main.2fe069fe.chunk.css"
+  },
+  {
+    "revision": "e47caf6f042803d40569",
+    "url": "/imageFinder/static/js/2.b7b0a00d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imageFinder/static/js/2.b7b0a00d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623dacd6f9c5e7e5daa5",
+    "url": "/imageFinder/static/js/main.a61068af.chunk.js"
+  },
+  {
+    "revision": "8ae4a88ba8a5f05865d1",
+    "url": "/imageFinder/static/js/runtime-main.004df1e3.js"
+  }
+]);
